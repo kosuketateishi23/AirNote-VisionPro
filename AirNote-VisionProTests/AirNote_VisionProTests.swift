c4fbd7051e2f950e4ce3bb8ff31e6f4b43b0ed3a
kosuketateishi23 <kosk.tate@gmail.com>
2025-05-14 12:13:29 +0900

    Fixed project structure and removed nested AirNote-VisionPro

---
+++ AirNote-VisionProTests/AirNote_VisionProTests.swift
@@ -0,0 +1,17 @@
+//
+//  AirNote_VisionProTests.swift
+//  AirNote-VisionProTests
+//
+//  Created by Kosuke Tateishi on 2025/05/07.
+//
+
+import Testing
+@testable import AirNote_VisionPro
+
+struct AirNote_VisionProTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ AirNote_VisionProApp.swift
@@ -0,0 +1,34 @@
+//
+//  AirNote_VisionProApp.swift
+//  AirNote-VisionPro
+//
+//  Created by Kosuke Tateishi on 2025/05/07.
+//
+
+import SwiftUI
+
+@main
+struct AirNote_VisionProApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+        .windowStyle(.volumetric)
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+    }
+}
